b9a9f96c53cda047cc810a8c3a59201eaf683674
eShadeyXO <45102320+eShadeyXO@users.noreply.github.com>
2019-03-28 13:08:30 -0400

    Add files via upload

---
+++ Prog 4/Prog1A/AscsTypeDescCost.cs
@@ -0,0 +1,43 @@
+﻿//Program 1A
+//CIS 200-76
+//Fall 2017
+//Due: 9/25/2017
+//GRADING ID: D1220
+//File: AscsTypeDescCost.cs
+//This class sorts the list of parcels first by type (ascending) and then by cost (descending)
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Prog1
+{
+    class AscsTypeDescCost : IComparer<Parcel>
+    {
+        //Precondition: None
+        //Postcondition: When item1 < item2 return a negative number. When item1 == item2 return zero. When item1 > item2 return 
+        //a positive number.
+        public int Compare(Parcel item1, Parcel item2)
+        {
+            string type1; //Stores item1 type
+            string type2; //Stores item2 type
+
+            if (item1 == null && item2 == null)
+                return 0;
+
+            if (item1 == null)
+                return -1;
+
+            if (item2 == null)
+                return 1;
+
+            type1 = item1.GetType().ToString();
+            type2 = item2.GetType().ToString();
+
+            if (type1 == type2)
+                return (item1.CalcCost()).CompareTo(item2.CalcCost());
+
+            return type1.CompareTo(type2);
+        }
+    }
+}
---
+++ Prog 4/Prog1A/DescByDestZip.cs
@@ -0,0 +1,35 @@
+﻿//Program 1A
+//CIS 200-76
+//Fall 2017
+//Due: 9/25/2017
+//GRADING ID: D1220
+//File: DescByDestZip.cs
+//This class sorts the list of parcels in descending order by destination zip.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Prog1
+{
+    class DescByDestZip : IComparer<Parcel>
+    {
+        // Precondition:  None
+        // Postcondition: When item1 < item2, method returns negative #
+        //                When item1 == item2, method returns zero
+        //                When item1 > item2, method returns positive #
+        public int Compare(Parcel item1, Parcel item2)
+        {
+            if (item1 == null && item2 == null)
+                return 0;
+
+            if (item1 == null)
+                return -1;
+
+            if (item2 == null)
+                return 1;
+
+            return (item2.DestinationAddress.Zip).CompareTo(item1.DestinationAddress.Zip);
+        }
+    }
+}
