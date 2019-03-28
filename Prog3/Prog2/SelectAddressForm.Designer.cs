f7587595d23ff22cf13db067e4632b118c1462b4
eShadeyXO <45102320+eShadeyXO@users.noreply.github.com>
2019-03-28 13:07:36 -0400

    Add files via upload

---
+++ Prog3/Prog2/SelectAddressForm.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace UPVApp
+{
+    partial class SelectAddressForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.selectAddressLabel = new System.Windows.Forms.Label();
+            this.editAddressCombo = new System.Windows.Forms.ComboBox();
+            this.editAddressButton = new System.Windows.Forms.Button();
+            this.editAddressErrorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.editAddressErrorProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // selectAddressLabel
+            // 
+            this.selectAddressLabel.AutoSize = true;
+            this.selectAddressLabel.Location = new System.Drawing.Point(30, 36);
+            this.selectAddressLabel.Name = "selectAddressLabel";
+            this.selectAddressLabel.Size = new System.Drawing.Size(296, 32);
+            this.selectAddressLabel.TabIndex = 0;
+            this.selectAddressLabel.Text = "Select address to edit:";
+            // 
+            // editAddressCombo
+            // 
+            this.editAddressCombo.FormattingEnabled = true;
+            this.editAddressCombo.Location = new System.Drawing.Point(333, 36);
+            this.editAddressCombo.Name = "editAddressCombo";
+            this.editAddressCombo.Size = new System.Drawing.Size(385, 39);
+            this.editAddressCombo.TabIndex = 1;
+            this.editAddressCombo.Validating += new System.ComponentModel.CancelEventHandler(this.editAddressCombo_Validating);
+            this.editAddressCombo.Validated += new System.EventHandler(this.editAddressCombo_Validated);
+            // 
+            // editAddressButton
+            // 
+            this.editAddressButton.Location = new System.Drawing.Point(284, 96);
+            this.editAddressButton.Name = "editAddressButton";
+            this.editAddressButton.Size = new System.Drawing.Size(227, 67);
+            this.editAddressButton.TabIndex = 2;
+            this.editAddressButton.Text = "Edit Address";
+            this.editAddressButton.UseVisualStyleBackColor = true;
+            this.editAddressButton.Click += new System.EventHandler(this.editAddressButton_Click);
+            // 
+            // editAddressErrorProvider
+            // 
+            this.editAddressErrorProvider.ContainerControl = this;
+            // 
+            // SelectAddressForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 185);
+            this.Controls.Add(this.editAddressButton);
+            this.Controls.Add(this.editAddressCombo);
+            this.Controls.Add(this.selectAddressLabel);
+            this.Name = "SelectAddressForm";
+            this.Text = "SelectAddressForm";
+            this.Load += new System.EventHandler(this.SelectAddressForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.editAddressErrorProvider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label selectAddressLabel;
+        private System.Windows.Forms.ComboBox editAddressCombo;
+        private System.Windows.Forms.Button editAddressButton;
+        private System.Windows.Forms.ErrorProvider editAddressErrorProvider;
+    }
+}
---
+++ Prog3/Prog2/SelectAddressForm.cs
@@ -0,0 +1,85 @@
+﻿// Program 3
+// CIS 200-76
+// Fall 2017
+// Due: 11/8/2017
+// Grading ID: D1220
+//Class contains functionality for editAddressForm.
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UPVApp
+{
+    public partial class SelectAddressForm : Form
+    {
+        public const int MIN_ADDRESSES = 2; // Minimum number of addresses needed
+
+        private List<Address> addressList;
+
+        public SelectAddressForm(List<Address> addresses)
+        {
+            InitializeComponent();
+
+            addressList = addresses;
+        }
+
+        internal int EditAddressIndex
+        {
+            //Precondition: None
+            //Postcondition: Returns selected index of edit address combo.
+            get { return editAddressCombo.SelectedIndex; }
+
+            //Precondition: Item must be chosen and must be smaller than address count.
+            //Postcondition: Selected index is set to value.
+            set
+            {
+                if ((value >= -1) && (value < addressList.Count))
+                    editAddressCombo.SelectedIndex = value;
+                else
+                    throw new ArgumentOutOfRangeException("EditAddressIndex", value,
+                        "Index must be valid.");
+            }
+        }
+
+        //Precondition: None
+        //Postcondition: editAddressCombo is loaded with available address names.
+        private void SelectAddressForm_Load(object sender, EventArgs e)
+        {
+                foreach (Address a in addressList)
+                {
+                    editAddressCombo.Items.Add(a.Name);
+                }
+        }
+
+        //Precondition: editAddressButton is activated.
+        //Postcondition: SelectAddressForm closes.
+        private void editAddressButton_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+        }
+
+        //Precondition: editAddressCombo must have a selected item.
+        //Postcondition: Correct error is displayed if no item is chosen.
+        private void editAddressCombo_Validating(object sender, CancelEventArgs e)
+        {
+            if (editAddressCombo.SelectedIndex == -1) // -1 means no item selected
+            {
+                e.Cancel = true;
+                editAddressErrorProvider.SetError(editAddressCombo, "Must select an address");
+            }
+        }
+
+        //Precondition: editAddressCombo passes validation.
+        //Postcondition: Error is cleared, if any.
+        private void editAddressCombo_Validated(object sender, EventArgs e)
+        {
+            editAddressErrorProvider.SetError(editAddressCombo, "");
+        }
+    }
+}
